710ecb301993f8451a0a408aa068a7147c3db33f
Renato <Renato@DESKTOP-AGPJAMV.(none)>
2023-04-13 13:39:36 -0300

    add context App

--- src/components/List/List.jsx
+++ src/components/List/List.jsx
@@ -1,23 +1,36 @@
-import { useMemo } from 'react';
+import { useCallback, useMemo } from 'react';
 import './List.css';
+import Heart from '../Heart/Heart';
+import { Link } from 'react-router-dom';
 
-function List({ items = []}) {
+function List({ items = [], favorites = [], onItemFavorite}) {
+
+	const handleOnHeartClick = useCallback ((value) => {
+		onItemFavorite && onItemFavorite(value);
+
+	}, [onItemFavorite]);
 
 	const itemsMap = useMemo(() => {
 		return items.map((pokemon)=> {
+			const isFAvorite = favorites.includes(pokemon.name);
 
 			const sprite = pokemon.sprites.other['official-artwork'].front_default;
 
 			return (
 				<div key={pokemon.name} className='list-item'>
+					<div className='list-item-heart-adapter'>
+						<Heart
+							selected={isFAvorite}
+							onClick={() => handleOnHeartClick(pokemon)}/>
+					</div>
 					<div className='list-item-image-wrapper'>
 						<img src={sprite} alt={pokemon.name} className='list-item-image'/>
 					</div>
-					<p>{pokemon.name}</p>
+					<Link to={`/details/${pokemon.name}`}>{pokemon.name}</Link>
 				</div>
 			);
 		});
-	}, [items]);
+	}, [items, handleOnHeartClick, favorites]);
 
 	return (
 		<div className='list-wrapper'>
---
+++ src/components/Router/Router.jsx
@@ -0,0 +1,22 @@
+import { Route, Routes } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
+import HomePage from '../../Pages/HomePage/HomePage';
+import DetailsPage from '../../Pages/DetailsPage/DetailsPage';
+
+function Router(){
+
+	return (
+		<BrowserRouter>
+			<Routes>
+				<Route path='/' element={<HomePage />} />
+				<Route path='/details/:name' element={<DetailsPage />} />
+				<Route path='*' element={<HomePage />} />
+			</Routes>
+		</BrowserRouter>
+
+	);
+
+
+}
+
+export default Router;
